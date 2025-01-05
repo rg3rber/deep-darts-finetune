d0b997bd7b5e3810c06a03f8226389cac6ef9fe0
Reto <rgerber@mailbox.org>
2025-01-05 12:34:27 +0100

    only take in files in combine labels

--- combine_labels.py
+++ combine_labels.py
@@ -2,15 +2,14 @@ import pandas as pd
 import os
 
 path = './dataset/annotations/'
-label_files = os.listdir(path)
+# Get list of files in the directory
+label_files = [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
 
 labels = []
-
 for label_file in label_files:
     df = pd.read_pickle(os.path.join(path, label_file))
     df["img_folder"] = label_file.split(".")[0]
     labels.append(df)
 
 labels = pd.concat(labels)
-
 pd.to_pickle(obj=labels, filepath_or_buffer="dataset/labels.pkl")
